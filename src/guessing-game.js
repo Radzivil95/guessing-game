0e4f94fdd64f02df75995a7ad4e84ad3390c9a27
VladRadzivil <vladradziviloff@gmail.com>
2023-02-27 17:29:52 +0100

    feat:task complite

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+    this.min = min;
+    this.max = max;
     }
 
     guess() {
-
+        return this.guessNum = Math.round((this.max + this.min) / 2);
     }
 
     lower() {
-
+        return this.max = this.guessNum;
     }
 
     greater() {
-
+        return this.min = this.guessNum;
     }
 }
 
